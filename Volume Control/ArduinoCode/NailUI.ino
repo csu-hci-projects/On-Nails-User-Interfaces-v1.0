10c683349951a9ab09b9ece5f6057ad9512c76f2
Minjie Shen <33135945+mjshen809@users.noreply.github.com>
2019-04-13 06:01:07 -0600

    Add files via upload

---
+++ Volume Control/ArduinoCode/NailUI.ino
@@ -0,0 +1,79 @@
+//Example sketch for RFID ID-20LA reader
+//More info available at http://tutorial.cytron.com.my/
+
+byte tagID[12];
+boolean tagread = false;
+int reset = 14;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(reset, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(reset, HIGH);
+  int incomingByte = 0;
+
+  if(Serial.available()>= 13)      //Make sure all the frame is received
+  {
+    if(Serial.read()== 0x02)       //Check for the start byte = 0x02
+    {
+      tagread = true;                //New tag is read
+      for(int index = 0; index < sizeof(tagID); index++)
+      {
+        byte val = Serial.read();
+        //convert from ASCII value to value range from 0 to 9 and A to F
+        if( val >= '0' && val <= '9')
+          val = val - '0';
+        else if( val >= 'A' && val <= 'F')
+          val = 10 + val - 'A';
+        tagID[index] = val;
+      }
+    }
+    else
+    {
+      tagread = false;                //Discard and skip
+    }
+  }
+  
+  if(tagread == true)                 //New tag is read
+  {
+    //print_tag();                    //Display the tag ID
+    byte target_tag_up[10] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2, 0x9, 0xB};
+    byte target_tag_down[10] = {0x0, 0x0, 0x7, 0x8, 0x4, 0xA, 0x1, 0x6, 0x0, 0x6};
+    
+    if (memcmp(tagID, target_tag_up, 10) == 0){
+      Serial.println("U");  
+    } else if (memcmp(tagID, target_tag_down, 10) == 0){
+      Serial.println("D"); 
+    } else {
+      Serial.println("M"); 
+    }
+    clear_tag();                    //Clear the tag ID and ready for next tag
+    tagread = false;
+  }
+  delay(10);
+  
+}
+
+void print_tag()                    //Subroutine to display the tagID content
+{
+  Serial.print("Tag ID: ");        
+  for(int index = 0; index < 10; index++)
+  {
+    Serial.print(tagID[index],HEX);  //Display tagID
+  }
+  Serial.print("\r\nChecksum: ");
+  Serial.print(tagID[10],HEX);       //Display checksum
+  Serial.println(tagID[11],HEX);
+}
+
+void clear_tag()                    //Subroutine to clear the tagID content
+{  
+  for(int index = 0; index < sizeof(tagID); index++)
+  {
+    tagID[index] = 0;
+  }
+}
---
+++ Volume Control/WindowsApplicationVBCode/VolumeControl/Panel.vb
@@ -0,0 +1,63 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class VolumeControl
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)> Private Shared Function SendMessage(ByVal hWnd As IntPtr, ByVal Msg As UInteger, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As IntPtr
+    End Function
+
+    Const WM_APPCOMMAND As UInteger = &H319
+    Const APPCOMMAND_VOLUME_UP As UInteger = &HA
+    Const APPCOMMAND_VOLUME_DOWN As UInteger = &H9
+    Const APPCOMMAND_VOLUME_MUTE As UInteger = &H8
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SerialPort1.Close()
+        SerialPort1.PortName = "com4"
+        SerialPort1.BaudRate = 9600
+        SerialPort1.DataBits = 8
+        SerialPort1.Encoding = System.Text.Encoding.Default
+        SerialPort1.Open()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Dim his As Char = SerialPort1.ReadExisting()
+        If his = "D" Then 'turn down the volume
+            Try
+                SendMessage(Me.Handle, WM_APPCOMMAND, &H30292, APPCOMMAND_VOLUME_DOWN * &H10000)
+                Label1.Text = "Down"
+
+            Catch ex As Exception
+
+            End Try
+        End If
+
+        If his = "U" Then 'turn up the volume
+            Try
+                SendMessage(Me.Handle, WM_APPCOMMAND, &H30292, APPCOMMAND_VOLUME_UP * &H10000)
+                Label1.Text = "Up"
+
+            Catch ex As Exception
+
+            End Try
+
+        End If
+
+        If his = "M" Then 'mute
+            Try
+                SendMessage(Me.Handle, WM_APPCOMMAND, &H200EB0, APPCOMMAND_VOLUME_MUTE * &H10000)
+                Label1.Text = "Mute"
+            Catch ex As Exception
+
+            End Try
+
+        End If
+
+    End Sub
+
+
+End Class
+
+
+
+
