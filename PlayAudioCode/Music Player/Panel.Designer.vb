44ac53cf7b07f81f0d1cb6e629285d944cbba461
Minjie Shen <33135945+mjshen809@users.noreply.github.com>
2019-05-10 23:06:15 -0600

    Full VB folder

---
+++ PlayAudioCode/Music Player/Panel.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MusicPlayer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM3"
+        Me.SerialPort1.RtsEnable = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(16, 21)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(8, 0, 8, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 69)
+        Me.Label1.TabIndex = 12
+        '
+        'MusicPlayer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 32.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.BackColor = System.Drawing.Color.GhostWhite
+        Me.ClientSize = New System.Drawing.Size(318, 202)
+        Me.Controls.Add(Me.Label1)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Font = New System.Drawing.Font("Arial", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(8, 6, 8, 6)
+        Me.Name = "MusicPlayer"
+        Me.Text = "Volume Control"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+End Class
---
+++ PlayAudioCode/Music Player/Panel.vb
@@ -0,0 +1,96 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class MusicPlayer
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)> Private Shared Function SendMessage(ByVal hWnd As IntPtr, ByVal Msg As UInteger, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As IntPtr
+    End Function
+
+    Const WM_APPCOMMAND As UInteger = &H319
+    Const APPCOMMAND_VOLUME_UP As UInteger = &HA
+    Const APPCOMMAND_VOLUME_DOWN As UInteger = &H9
+    Const APPCOMMAND_VOLUME_MUTE As UInteger = &H8
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SerialPort1.Close()
+        SerialPort1.PortName = "com4"
+        SerialPort1.BaudRate = 9600
+        SerialPort1.DataBits = 8
+        SerialPort1.Encoding = System.Text.Encoding.Default
+        SerialPort1.Open()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Dim his As Char = SerialPort1.ReadExisting()
+        Dim i, j As Integer
+        Dim songs() As String = {"D:\Coldplay - Yellow.wav", "D:\Coldplay - Viva La Vida.wav",
+          "D:\Linkin Park - Numb.wav"}
+        Static songIndex As Integer = 0
+        Static playOrStop As Integer = 0
+
+        If his = "D" Then 'turn down the volume
+            Try
+                For i = 0 To 4
+                    SendMessage(Me.Handle, WM_APPCOMMAND, &H30292, APPCOMMAND_VOLUME_DOWN * &H10000)
+                    Label1.Text = "Down"
+                Next i
+            Catch ex As Exception
+            End Try
+
+        End If
+
+        If his = "U" Then 'turn up the volume
+            Try
+                For j = 0 To 4
+                    SendMessage(Me.Handle, WM_APPCOMMAND, &H30292, APPCOMMAND_VOLUME_UP * &H10000)
+                    Label1.Text = "Up"
+                Next j
+            Catch ex As Exception
+            End Try
+        End If
+
+        If his = "M" Then 'mute
+
+            Try
+                SendMessage(Me.Handle, WM_APPCOMMAND, &H200EB0, APPCOMMAND_VOLUME_MUTE * &H10000)
+                Label1.Text = "Mute"
+            Catch ex As Exception
+            End Try
+        End If
+
+        If his = "P" Then 'play music, stop music alternatively
+
+            playOrStop = playOrStop Mod 2
+
+            If playOrStop = 0 Then
+                My.Computer.Audio.Play("D:\Coldplay - Yellow.wav",
+                AudioPlayMode.BackgroundLoop)
+                Label1.Text = "Music: " + "D:\Coldplay - Yellow.wav"
+            Else
+                My.Computer.Audio.Stop()
+                Label1.Text = "Music stop"
+            End If
+
+            playOrStop = playOrStop + 1
+
+        End If
+
+
+        If his = "S" Then 'switch songs
+
+            songIndex = songIndex + 1
+            songIndex = songIndex Mod 3
+            My.Computer.Audio.Play(songs(songIndex), AudioPlayMode.BackgroundLoop)
+            Label1.Text = "Switch to: " + songs(songIndex)
+
+        End If
+
+
+    End Sub
+
+
+End Class
+
+
+
+
